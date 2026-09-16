994e965ab3f6defdcc89945225791a74e5c321f0
Priangsu Saha <179412089+PriangSu-Who@users.noreply.github.com>
2026-09-16 16:29:25 +0600

    Prepare MediaNest v1.3.0 release

--- MediaNest.cmd
+++ MediaNest.cmd
@@ -3,9 +3,5 @@ setlocal
 cd /d "%~dp0"
 title MediaNest
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0MediaNest.ps1"
-if errorlevel 1 (
-  echo.
-  echo MediaNest closed because of an error.
-  pause
-)
-endlocal
+set "exitCode=%ERRORLEVEL%"
+endlocal & exit /b %exitCode%
